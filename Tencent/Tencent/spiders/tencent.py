80c69d65c0bd73365478f580d3d8ebdd75adf0f0
eerbabao <1292124563@qq.com>
2017-08-21 20:52:59 +0800

    update

---
+++ Tencent/Tencent/spiders/tencent.py
@@ -0,0 +1,29 @@
+# -*-coding: uft-8 -*-
+import scrapy
+from Tencent.items import TencentItem
+
+class TencentSpider(scrapy,Spider):
+    name = 'tencent'
+    allowed_domains = ['tencent.com']
+    baseURL = "http://hr.tencent.com/position.php?start="
+    offset = 0
+    start_urls = [baseURL + str(offset)]
+
+    def parse(self, response):
+        node_list = response.xpath("//tr[@class='even'] | //tr[@class='odd']")
+
+        for node in node_list:
+            item = TencentItem()
+            # 提取每个职位的相关信息并转码为utf-8编码
+            item['positionName'] = node.xpath("./a/text()").extract()[0].encode("utf-8")
+            item['positionLink'] = node.xpath("./a/@href").extract()[0].encode("utf-8")
+            item['positionType'] = node.xpath("./td[2]/text()").extract()[0].encode("utf-8")
+            item['peopleNumber'] = node.xpath("./td[3]/text()").extract()[0].encode("utf-8")
+            item['workLocation'] = node.xpath("./td[4]/text()").extract()[0].encode("utf-8")
+            item['publishTime'] = node.xpath("./td[5]/text()").extract()[0].encode("utf-8")
+
+            yield item
+        if self.offset <= 2190
+            self.offset += 10
+            url = self.baseURL + self.offset
+            yield.scrapy.Request(url, callback = self.parse)
