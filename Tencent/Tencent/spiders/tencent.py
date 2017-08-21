9516992600ca33f656248e56535eac08921294c0
eerbabao <1292124563@qq.com>
2017-08-21 21:40:13 +0800

    update

--- Tencent/Tencent/spiders/tencent.py
+++ Tencent/Tencent/spiders/tencent.py
@@ -1,8 +1,8 @@
-# -*-coding: uft-8 -*-
+# -*- coding: utf-8 -*-
 import scrapy
 from Tencent.items import TencentItem
 
-class TencentSpider(scrapy,Spider):
+class TencentSpider(scrapy.Spider):
     name = 'tencent'
     allowed_domains = ['tencent.com']
     baseURL = "http://hr.tencent.com/position.php?start="
@@ -15,15 +15,18 @@ class TencentSpider(scrapy,Spider):
         for node in node_list:
             item = TencentItem()
             # 提取每个职位的相关信息并转码为utf-8编码
-            item['positionName'] = node.xpath("./a/text()").extract()[0].encode("utf-8")
-            item['positionLink'] = node.xpath("./a/@href").extract()[0].encode("utf-8")
-            item['positionType'] = node.xpath("./td[2]/text()").extract()[0].encode("utf-8")
+            item['positionName'] = node.xpath(".//a/text()").extract()[0].encode("utf-8")
+            item['positionLink'] = node.xpath(".//a/@href").extract()[0].encode("utf-8")
+            if len(node.xpath("./td[2]/text()")):
+                item['positionType'] = node.xpath("./td[2]/text()").extract()[0].encode("utf-8")
+            else:
+                item['positionType'] = ""
             item['peopleNumber'] = node.xpath("./td[3]/text()").extract()[0].encode("utf-8")
             item['workLocation'] = node.xpath("./td[4]/text()").extract()[0].encode("utf-8")
             item['publishTime'] = node.xpath("./td[5]/text()").extract()[0].encode("utf-8")
 
             yield item
-        if self.offset <= 2190
+        if self.offset <= 2270:
             self.offset += 10
-            url = self.baseURL + self.offset
-            yield.scrapy.Request(url, callback = self.parse)
+            url = self.baseURL + str(self.offset)
+            yield scrapy.Request(url, callback = self.parse)
